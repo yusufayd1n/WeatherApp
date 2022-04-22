b955acb517bc9420024b91a34ec263af59afcd37
sahin <yusufaydn24@gmail.com>
2022-04-23 02:38:34 +0300

    API removed for security.

--- app/src/main/java/com/example/weatherapp/service/API.kt
+++ app/src/main/java/com/example/weatherapp/service/API.kt
@@ -6,8 +6,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface API {
-    //https://api.openweathermap.org/data/2.5/weather?q=trabzon&appid=fadfe3bd9c688cd82830ca2cdceaeb09
-    @GET("data/2.5/weather?&units=metric&appid=fadfe3bd9c688cd82830ca2cdceaeb09")
+    @GET("data/2.5/weather?&units=metric&appid=API_KEY")
     fun getData(
         @Query("q") cityName:String
     ):Single<ModelWeather>
--- app/src/main/java/com/example/weatherapp/service/APIService.kt
+++ app/src/main/java/com/example/weatherapp/service/APIService.kt
@@ -7,7 +7,6 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class APIService {
-    ////https://api.openweathermap.org/data/2.5/weather?q=izmir&appid=fadfe3bd9c688cd82830ca2cdceaeb09
     private val BASE_URL="https://api.openweathermap.org/"
     private val api=Retrofit.Builder()
         .baseUrl(BASE_URL)
